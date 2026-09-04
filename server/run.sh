b28e6748bf234fcc88d908fb9bba7a0eb19aa0f4
beltrewilton <beltre.wilton@gmail.com>
2026-09-03 20:28:22 -0400

    conf files

--- server/run.sh
+++ server/run.sh
@@ -5,7 +5,7 @@ export PATH="$HOME/.elixir-install/installs/otp/27.1.2/bin:$PATH"
 export PATH="$HOME/.elixir-install/installs/elixir/1.17.2-otp-27/bin:$PATH"
 export MIX_ENV=prod
 
-APP_DIR="/opt/pos-app"
+APP_DIR="/opt/pos-app/server"
 NODE_NAME="posserver@138.197.112.92"
 
 cd "$APP_DIR"
